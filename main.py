f042a30b793e88d02f5863d2eecab2876d1d3648
cvafaian <cvafaian@umich.edu>
2021-05-15 08:42:19 -0400

    Final changes

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
-import sys, os
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QPushButton, QMessageBox, QScrollArea
+import subprocess
+import sys
+
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
-import subprocess
+from PyQt5.QtWidgets import (
+    QApplication,
+    QLabel,
+    QMessageBox,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
+
 
 class ImageLabel(QLabel):
     def __init__(self):
         super().__init__()
 
         self.setAlignment(Qt.AlignCenter)
-        self.setText('\n\n Drop Image Here \n\n')
-        self.setStyleSheet('''
+        self.setText("\n\n Drop Image Here \n\n")
+        self.setStyleSheet(
+            """
             QLabel{
                 border: 4px dashed #aaa
             }
-        ''')
+        """
+        )
 
     def setPixmap(self, image):
         super().setPixmap(image)
@@ -23,6 +34,7 @@ class ImageLabel(QLabel):
 class App(QWidget):
     def __init__(self):
         super().__init__()
+        self.alternative = False
         self.brief = "No changes have been made."
         self.text = "No image has been changed yet.\n\nDrag and drop image into box to remove metadata and generate information."
         self.resize(400, 400)
@@ -33,8 +45,10 @@ class App(QWidget):
         self.photoViewer = ImageLabel()
         mainLayout.addWidget(self.photoViewer)
 
-        button = QPushButton('Show Metadata', self)
-        button.setToolTip('This will show image metadata from before and after it is changed')
+        button = QPushButton("Show Metadata", self)
+        button.setToolTip(
+            "This will show image metadata from before and after it is changed"
+        )
         mainLayout.addWidget(button)
 
         button.clicked.connect(self.clickEvent)
@@ -59,13 +73,10 @@ class App(QWidget):
             file_path = event.mimeData().urls()[0].toLocalFile()
             self.success(file_path)
             event.accept()
-
         else:
             event.ignore()
 
     def clickEvent(self):
-
-        # msgBox = ScrollMessageBox(self.text)
         msgBox = QMessageBox()
         msgBox.setIcon(QMessageBox.Information)
         msgBox.setText(self.brief)
@@ -74,29 +85,33 @@ class App(QWidget):
         msgBox.setStandardButtons(QMessageBox.Ok)
         msgBox.exec()
 
-
     def success(self, file_path):
-        self.photoViewer.setPixmap(QPixmap("images/success.png"))
+        if self.alternative:
+            self.photoViewer.setPixmap(QPixmap("images/alt-success.png"))
+            self.alternative = False
+        else:
+            self.photoViewer.setPixmap(QPixmap("images/success.png"))
+            self.alternative = True
+
         self.removeMeta(file_path)
 
     def removeMeta(self, file_path):
         text = "-------------------------------------------"
         text += "\n\n Metadata Present Before: \n\n"
-        output = subprocess.run(["exiftool",  "-n", file_path], capture_output=True)
+        output = subprocess.run(["exiftool", "-n", file_path], capture_output=True)
         text += output.stdout.decode("utf-8")
         text += "\n\n Metadata Present After: \n\n"
-        output = subprocess.run(["exiftool",  "-all=", file_path], capture_output=True)
+        output = subprocess.run(["exiftool", "-all=", file_path], capture_output=True)
         text += output.stdout.decode("utf-8")
         self.brief = output.stdout.decode("utf-8")
         text += "\n"
-        output = subprocess.run(["exiftool",  "-n", file_path], capture_output=True)
+        output = subprocess.run(["exiftool", "-n", file_path], capture_output=True)
         text += output.stdout.decode("utf-8")
         text += "\n\n-------------------------------------------"
         self.text = text
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     run = App()
     run.show()
